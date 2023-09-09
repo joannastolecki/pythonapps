4499eb1f28493162ba280eacde8d61290d90fe71
iAka-fut <Joanna.stolecki@gmail.com>
2023-09-09 12:31:29 +0200

    screenshot

--- .venv/screenshot.py
+++ .venv/screenshot.py
@@ -1,1 +1,21 @@
-import 
+# pip install pyautogui
+#  pip install time
+
+import time
+import pyautogui
+
+# function create 
+def screenshot():
+    name = int(round(time.time() * 1000))
+    print(name)
+    name = '/Users/iaska/github clones/pythonapps/.venv/{}.png'.format(name)
+    print(name)
+    time.sleep(5) # delay excecution
+    img = pyautogui.screenshot(name)
+    img.show()
+    
+    
+screenshot()
+    
+    
+    
